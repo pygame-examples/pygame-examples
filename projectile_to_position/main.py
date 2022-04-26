af23915452db7ffe648ad7086e722e2f8b9f96f5
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-04-26 12:37:29 +0300

    Updated example, separated main class in it's own file

--- projectile_to_position/main.py
+++ projectile_to_position/main.py
@@ -1,36 +1,13 @@
 import pygame
 
+from projectile import Projectile
+
 
 pygame.init()
 screen = pygame.display.set_mode((500, 400))
 clock = pygame.time.Clock()
 
-
-class Bullet(pygame.sprite.Sprite):
-    image = pygame.Surface((10, 10))
-    image.fill('red')
-    velocity = 5
-    max_distance = 200
-
-    def __init__(self, start_pos, target_pos, to_max=True):
-        super().__init__()
-        self.start_pos = sp = x0, y0 = pygame.Vector2(start_pos)
-        tp = x1, y1 = pygame.Vector2(target_pos)
-
-        self.rect = self.image.get_rect(center=start_pos)
-        self.vector = pygame.Vector2(x1 - x0, y1 - y0).normalize()
-        if not to_max:
-            self.max_distance = min(self.max_distance, round(sp.distance_to(tp)))
-        self.travelled_distance = 0
-
-    def update(self):
-        if self.travelled_distance > self.max_distance:
-            self.kill()
-        self.travelled_distance += self.velocity
-        self.rect.center = self.start_pos + self.vector*self.travelled_distance
-
-
-bullet_group = pygame.sprite.Group()
+projectile_group = pygame.sprite.Group()
 
 running = True
 while running:
@@ -42,9 +19,9 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                bullet = Bullet((250, 200), event.pos)
-                bullet_group.add(bullet)
+                projectile = Projectile((250, 200), event.pos)
+                projectile_group.add(projectile)
 
-    bullet_group.update()
-    bullet_group.draw(screen)
+    projectile_group.update()
+    projectile_group.draw(screen)
     pygame.display.flip()
---
+++ projectile_to_position/projectile.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+class Projectile(pygame.sprite.Sprite):
+    image = pygame.Surface((20, 10), flags=pygame.SRCALPHA)
+    pygame.draw.polygon(image, 'red', ((0, 0), (15, 0), (20, 5), (15, 10), (0, 10)))
+    velocity = 5
+    max_distance = 200
+
+    def __init__(self, start_pos, target_pos, to_max=True):
+        super().__init__()
+        self.start_pos = sp = x0, y0 = pygame.Vector2(start_pos)
+        tp = x1, y1 = pygame.Vector2(target_pos)
+
+        self.rect = self.image.get_rect(center=start_pos)
+        self.vector = pygame.Vector2(x1 - x0, y1 - y0).normalize()
+        angle = self.vector.angle_to(pygame.Vector2(1, 0))
+        self.image = pygame.transform.rotate(self.image, angle)
+        if not to_max:
+            self.max_distance = min(self.max_distance, round(sp.distance_to(tp)))
+        self.travelled_distance = 0
+
+    def update(self):
+        if self.travelled_distance > self.max_distance:
+            self.kill()
+        self.travelled_distance += self.velocity
+        self.rect.center = self.start_pos + self.vector*self.travelled_distance
