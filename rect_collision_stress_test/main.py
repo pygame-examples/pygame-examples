cbe52a2b2c87561bdd4b0c5650a2178a9b7e2d9d
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-04-29 22:31:28 +0300

    Put code together in main

--- rect_collision_stress_test/main.py
+++ rect_collision_stress_test/main.py
@@ -1,75 +1,65 @@
+import asyncio
+
 import pygame
-import random
+
+from .reddy import Reddy
+from .UI import HorizontalSlider
 
 
 pygame.init()
-screen = pygame.display.set_mode((500, 400))
-clock = pygame.time.Clock()
-
-(RED_SURF := pygame.Surface((10, 10))).fill('red')
-(GREEN_SURF := pygame.Surface((10, 10))).fill('green')
 
 
-class Reddy(pygame.sprite.Sprite):
+async def main():
+    screen = pygame.display.set_mode((500, 400))
+    clock = pygame.time.Clock()
 
-    vector = pygame.Vector2(1, 0)
+    group = pygame.sprite.Group()
 
-    def __init__(self, *groups):
-        super().__init__(*groups)
-        self.image = RED_SURF
-        self.rect = self.image.get_rect(center=(250, 200))
-        self.angle = random.randint(-180, 180)
+    def spawn_entities(number):
+        group.empty()
+        for _ in range(number):
+            entity = Reddy()
+            group.add(entity)
 
-    def update(self):
-        collided = False
-        self.rect.move_ip(self.vector.rotate(self.angle) * 3)
-        for gr in self.groups():
-            for sprite in gr:
-                if self is sprite:
-                    continue
-                if self.rect.colliderect(sprite.rect):
-                    collided = True
+    horizontal_slider = HorizontalSlider(pygame.Rect(50, 350, 400, 40), callback=spawn_entities)
 
-        if self.rect.top < 0:
-            self.angle = random.randint(0, 180)
-        elif self.rect.bottom > 400:
-            self.angle = random.randint(-180, 0)
-        elif self.rect.left < 0:
-            self.angle = random.randint(-90, 90)
-        elif self.rect.right > 500:
-            self.angle = random.randint(90, 270)
+    font = pygame.font.Font(None, 16)
 
-        if collided:
-            self.image = GREEN_SURF
-        elif not collided:
-            self.image = RED_SURF
+    running = True
+    while running:
+        clock.tick(60)
+        screen.fill((0, 0, 0))
+
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+
+        group.update()
+        group.draw(screen)
+
+        horizontal_slider.update(events)
+        horizontal_slider.draw(screen)
+
+        fps_text = font.render(f'FPS: {clock.get_fps():.0f}', True, 'white')
+        fps_rect = fps_text.get_rect()
+        entity_count = len(group)
+        check_text = font.render(f'Collision checks: {entity_count * (entity_count-1):,}', True, 'white')
+        slider_value = font.render(f'Slider value (entities): {horizontal_slider.current_value}', True, 'white')
+        screen.blits((
+            (fps_text, fps_rect), (check_text, check_text.get_rect(top=fps_rect.bottom)),
+            (slider_value, slider_value.get_rect(top=check_text.get_rect(top=fps_rect.bottom).bottom))
+        ))
+
+        pygame.display.flip()
+
+    pygame.quit()
+    await asyncio.sleep(0)
 
 
-group = pygame.sprite.Group()
+def run():
+    asyncio.run(main())
 
-entity_count = 200
-for _ in range(entity_count):
-    Reddy(group)
 
-font = pygame.font.Font(None, 16)
-check_text = font.render(f'{entity_count ** 2:,}', True, 'white')
-
-running = True
-while running:
-    clock.tick(60)
-    screen.fill((0, 0, 0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    group.update()
-    group.draw(screen)
-
-    fps_text = font.render(f'FPS: {clock.get_fps():.0f}', True, 'white')
-    fps_rect = fps_text.get_rect()
-    screen.blits(((fps_text, fps_rect), (check_text, check_text.get_rect(top=fps_rect.bottom))))
-
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == '__main__':
+    run()
