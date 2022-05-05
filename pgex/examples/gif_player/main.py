0e30d9b527ee9a31193dc8dcd570fe89e4907b44
Matiiss <83066658+Matiiss@users.noreply.github.com>
2022-05-06 00:24:42 +0300

    Fixed path not found and added WASM support

--- pgex/examples/gif_player/main.py
+++ pgex/examples/gif_player/main.py
@@ -1,3 +1,6 @@
+import asyncio
+import os
+
 import pygame
 from .gif_player import GIFPlayer
 
@@ -5,20 +8,38 @@ from .gif_player import GIFPlayer
 WIDTH, HEIGHT = 500, 400
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
 
-background_gif = GIFPlayer("./gif_player/image.gif", size=(WIDTH, HEIGHT), exclude=(0,))
 
-running = True
-while running:
-    clock.tick(60)
-    screen.fill("black")
+async def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
 
-    background_gif.play(screen)
+    background_gif = GIFPlayer(
+        os.path.join(
+            os.path.dirname(__file__), "image.gif"
+        ),
+        size=(WIDTH, HEIGHT),
+        exclude=(0,)
+    )
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    running = True
+    while running:
+        clock.tick(60)
+        screen.fill("black")
 
-    pygame.display.flip()
+        background_gif.play(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        await asyncio.sleep(0)
+
+
+def run():
+    asyncio.run(main())
+
+
+if __name__ == '__main__':
+    run()
