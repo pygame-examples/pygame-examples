e322460fb210707ede830f8a376b5871b1f67aae
Avaxar <44055981+avaxar@users.noreply.github.com>
2022-05-08 17:41:23 +0700

    Rename `globals.py` to `common.py`

--- pgex/examples/parallax_background/main.py
+++ pgex/examples/parallax_background/main.py
@@ -4,7 +4,7 @@ import time
 
 import pygame
 
-from . import globals
+from . import common
 
 
 def load_image(file: str) -> pygame.Surface:
@@ -14,18 +14,18 @@ def load_image(file: str) -> pygame.Surface:
 
 
 async def main():
-    globals.screen = pygame.display.set_mode((800, 600))
-    globals.clock = pygame.time.Clock()
-    globals.running = True
+    common.screen = pygame.display.set_mode((800, 600))
+    common.clock = pygame.time.Clock()
+    common.running = True
 
     # Load background assets
-    globals.inner_cave_bg = load_image("inner_cave.png")
-    globals.outer_cave_bg = load_image("outer_cave.png")
-    globals.plains_bg = load_image("plains.png")
-    globals.mountains_bg = load_image("mountains.png")
-    globals.sky_bg = load_image("sky.png")
+    common.INNER_CAVE_BG = load_image("inner_cave.png")
+    common.OUTER_CAVE_BG = load_image("outer_cave.png")
+    common.PLAINS_BG = load_image("plains.png")
+    common.MOUNTAINS_BG = load_image("mountains.png")
+    common.SKY_BG = load_image("sky.png")
 
-    while globals.running:
+    while common.running:
         loop()
         await asyncio.sleep(0)
 
@@ -34,7 +34,7 @@ def loop():
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
-            running = False
+            common.running = False
 
     # As if we're moving 500 pixels per second to the right.
     # Relative to the camera, the backgrounds should move left --
@@ -43,18 +43,18 @@ def loop():
 
     # Draw the backgrounds in order from the furthest to the nearest.
     for background, depth in [
-        (globals.sky_bg, 50),
-        (globals.mountains_bg, 20),
-        (globals.plains_bg, 10),
-        (globals.outer_cave_bg, 5),
-        (globals.inner_cave_bg, 1),
+        (common.SKY_BG, 50),
+        (common.MOUNTAINS_BG, 20),
+        (common.PLAINS_BG, 10),
+        (common.OUTER_CAVE_BG, 5),
+        (common.INNER_CAVE_BG, 1),
     ]:
         draw_bg(background, bg_scroll, depth)
 
     pygame.display.flip()
-    globals.clock.tick(60)
+    common.clock.tick(60)
 
-    pygame.display.set_caption(f"FPS: {globals.clock.get_fps():.2f}")
+    pygame.display.set_caption(f"FPS: {common.clock.get_fps():.2f}")
 
 
 def draw_bg(background: pygame.Surface, scroll: float, depth: float):
@@ -67,13 +67,13 @@ def draw_bg(background: pygame.Surface, scroll: float, depth: float):
 
     # We want to be as efficient with our calls as possible.
     # If `x` is not out of the window, we can draw the main piece of the background.
-    if x <= globals.screen.get_width():
-        globals.screen.blit(background, (x, 0))
+    if x <= common.screen.get_width():
+        common.screen.blit(background, (x, 0))
 
     # We need the background to loop; it happens that the back part gets left out.
     # Unless the main piece had covered it (x == 0), we'll draw it behind the main piece.
     if x != 0:
-        globals.screen.blit(background, (x - background.get_width(), 0))
+        common.screen.blit(background, (x - background.get_width(), 0))
 
 
 def run():
