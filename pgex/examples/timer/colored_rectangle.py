cd4b3e5e4a693cb0c571eca597bfe5dbdee0bd61
coinfu <97292654+pycoinfu@users.noreply.github.com>
2022-05-08 14:28:42 +0800

    added timer example

---
+++ pgex/examples/timer/colored_rectangle.py
@@ -0,0 +1,34 @@
+import pygame
+import itertools
+from typing import Iterable
+from ._types import ColorValue
+
+
+class ColoredRect:
+    """
+    A colored rectangle class to demonstrate the usage of pygame timers
+    """
+
+    def __init__(
+        self, pos: Iterable[int], size: Iterable[int], colors: Iterable[ColorValue]
+    ):
+        """
+        Parameters:
+                pos: The position of the rectangle
+                size: The size of the rectangle
+                colors: An iterable containing rectangle's colors
+        """
+
+        self.rect = pygame.Rect(pos, size)
+        # create a cycle so the colors change infinitely
+        self.color_cycle = itertools.cycle(colors)
+        self.color = next(self.color_cycle)
+
+    def change_color(self):
+        """
+        Change the color to the next one in the self.color_cycle
+        """
+        self.color = next(self.color_cycle)
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.rect(screen, self.color, self.rect)
