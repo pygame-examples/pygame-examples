2ec4c515cae6b567c33512a213c9f3ec9f55e123
Axis <77634274+blankRiot96@users.noreply.github.com>
2022-05-10 15:33:26 +0530

    Update __init__.py

--- pgex/examples/rect_collision_stress_test/__init__.py
+++ pgex/examples/rect_collision_stress_test/__init__.py
@@ -3,6 +3,6 @@ This file is a part of the 'Pygame Examples (pgex)' source code.
 The source code is distributed under the MIT license.
 """
 
-import rect_collision_stress_test.main
+from .main import run
 
-rect_collision_stress_test.main.run()
+run()
