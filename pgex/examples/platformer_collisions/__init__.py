7beaad201e06494ef8855b02ebe0b9054a49a628
ScriptLineStudios <scriptlinestudios@protonmail.com>
2022-06-16 08:48:22 +0200

    refactored to use OOP

--- pgex/examples/platformer_collisions/__init__.py
+++ pgex/examples/platformer_collisions/__init__.py
@@ -3,6 +3,7 @@ This file is a part of the 'Pygame Examples (pgex)' source code.
 The source code is distributed under the MIT license.
 """
 
-from .main import run
+from .main import Game
 
-run()
+game = Game()
+game.run()
--- pgex/examples/platformer_collisions/main.py
+++ pgex/examples/platformer_collisions/main.py
@@ -8,118 +8,60 @@ import asyncio
 import pygame
 
 from . import common
+from .player import Player
+from .tile import Tile
 
 
-def get_colliding_tiles(tiles: list, player_rect: pygame.Rect) -> list:
-    """
-    Returns a list of tiles the player is currently colliding with
-    """
-    return_tiles = []
-    for tile in tiles:
-        if player_rect.colliderect(tile):
-            return_tiles.append(tile)
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
 
-    return tiles
+        self.display = pygame.display.set_mode(common.DIMENSIONS)
+        pygame.display.set_caption("Platformer Collisions Example")
 
+        self.clock = pygame.time.Clock()
+        self.key_presses = {"a": False, "d": False}
+        self.player = Player(x=100, y=100, width=64, height=45, color=(255, 0, 0))
 
-def calculate_rect(
-    movement: list, player_rect: pygame.Rect, map_tiles: list
-) -> pygame.Rect:
-    """
-    Calculates the Rect of the player based on their movement and the surrounding tiles
-    """
-    player_rect.x += movement[0]
-    tiles = get_colliding_tiles(map_tiles, player_rect)
-    for tile in tiles:
-        if player_rect.colliderect(tile):
-            if movement[0] > 0:
-                player_rect.right = pygame.Rect(tile[0], tile[1], tile[2], tile[3]).left
-            if movement[0] < 0:
-                player_rect.left = pygame.Rect(tile[0], tile[1], tile[2], tile[3]).right
+    def render_map(self, display: pygame.Surface, tiles: list[Tile]) -> None:
+        """
+        Renders the games tiles
+        """
 
-    common.is_on_ground = False
-    player_rect.y += movement[1]
-    tiles = get_colliding_tiles(map_tiles, player_rect)
-    for tile in tiles:
-        if player_rect.colliderect(tile):
-            if movement[1] > 0:
-                player_rect.bottom = pygame.Rect(tile[0], tile[1], tile[2], tile[3]).top
-                common.is_on_ground = True
-                common.y_velocity = 3
-            if movement[1] < 0:
-                player_rect.top = pygame.Rect(tile[0], tile[1], tile[2], tile[3]).bottom
+        for tile in tiles:
+            pygame.draw.rect(self.display, (60, 255, 100), tile.rect)
 
-    return player_rect
+    async def main(self) -> None:
+        """
+        Game main function. Handles key presses, map and player rendering
+        """
+        while True:
+            self.display.fill(pygame.Color("black"))
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    raise SystemExit
 
-def player_movement(key_presses: dict, player_rect: pygame.Rect) -> pygame.Rect:
-    """
-    Handles all code realting to the movement of the player
-    """
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        if self.player.is_on_ground:
+                            self.player.y_velocity -= self.player.JUMP_HEIGHT
 
-    player_movement = [0, common.y_velocity]
-    if key_presses["a"]:
-        player_movement[0] -= 4
-    if key_presses["d"]:
-        player_movement[0] += 4
+            self.key_presses["a"] = pygame.key.get_pressed()[pygame.K_a]
+            self.key_presses["d"] = pygame.key.get_pressed()[pygame.K_d]
 
-    if common.y_velocity < 10:
-        common.y_velocity += 0.5
+            self.player.handle_movement(self.key_presses)
+            self.player.draw(self.display)
 
-    player_rect = calculate_rect(player_movement, player_rect, common.map_data["map"])
-    return player_rect
+            self.render_map(self.display, common.tiles)
 
+            pygame.display.flip()
+            await asyncio.sleep(0)
+            self.clock.tick(common.FPS)
 
-def render_map(display: pygame.Surface, tiles: list) -> None:
-    """
-    Renders the games tiles
-    """
+    def run(self) -> None:
+        """
+        Run the main loop; using asyncio for WASM compatibility
+        """
 
-    for tile in tiles:
-        pygame.draw.rect(display, (60, 255, 100), tile)
-
-
-def render_player(display: pygame.Surface, player_rect: pygame.Rect) -> None:
-    """
-    Render the player
-    """
-
-    pygame.draw.rect(display, (255, 255, 255), player_rect)
-
-
-async def main() -> None:
-    pygame.init()
-
-    display = pygame.display.set_mode(common.DIMENSIONS)
-    clock = pygame.time.Clock()
-
-    key_presses = {"a": False, "d": False}
-    player_rect = pygame.Rect(100, 100, 64, 45)
-
-    while True:
-        display.fill((0, 0, 0))
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                raise SystemExit
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    if common.is_on_ground:
-                        common.y_velocity -= 16
-
-        key_presses["a"] = pygame.key.get_pressed()[pygame.K_a]
-        key_presses["d"] = pygame.key.get_pressed()[pygame.K_d]
-
-        player_rect = player_movement(key_presses, player_rect)
-
-        render_player(display, player_rect)
-        render_map(display, common.map_data["map"])
-
-        pygame.display.flip()
-        await asyncio.sleep(0)
-        clock.tick(60)
-
-
-def run() -> None:
-    asyncio.run(main())
+        asyncio.run(self.main())
